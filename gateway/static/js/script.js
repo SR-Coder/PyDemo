ec218ada23f507c8355b788ba1965bb919e2885d
Jim Reeder <reederje@gmail.com>
2021-01-07 17:12:55 -0800

    updates to the login page

--- gateway/static/js/script.js
+++ gateway/static/js/script.js
@@ -73,4 +73,10 @@ document.addEventListener('DOMContentLoaded', function(event){
             event.preventDefault()
         }
     })
+    document.getElementById('open').addEventListener('click', function(event){
+        document.getElementById('leftMenu').style.width="200px";
+    })
+    document.getElementById('close').addEventListener('click', function(event){
+        document.getElementById('leftMenu').style.width="0px"
+    })
 })
